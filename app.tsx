2305fbb783a9ea78febbe7c70aa5e4ab1e5e4f9f
maxeozakh <maxeozakh@gmail.com>
2022-01-22 20:28:59 +0300

    watch mode for esbuild

--- app.tsx
+++ app.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react'
-import * as Server from 'react-dom/server'
+import * as React from 'react';
+import { render } from 'react-dom';
 
-let Greet = () => <h1>Hello there</h1>
-console.log(Server.renderToString(<Greet />))
+let Greet = () => <h1>Hello there</h1>;
+render(<Greet />);
