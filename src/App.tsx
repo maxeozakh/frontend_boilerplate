3ed9b36f9248f1ab11a84e3fc88201853fe71f5c
Maxim Zakharov <maxeozakh@gmail.com>
2022-10-18 10:13:52 +0400

    handle images

--- src/App.tsx
+++ src/App.tsx
@@ -1,2 +1,9 @@
 import React from 'react'
-export const App = () => <div>hello</div>
+import image from './logo.jpg'
+
+export const App = () => (
+  <div>
+    <h3>hello</h3>
+    <img src={image} />
+  </div>
+)
